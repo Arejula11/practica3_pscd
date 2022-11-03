dca6ec7a8bee3383e36fc2a77c9d0b62c1e7d278
Miguel Aréjula Aisa <arejula10@gmail.com>
2022-11-03 11:35:55 +0100

    traduccion await ( error)

--- practica_3.cpp
+++ practica_3.cpp
@@ -36,7 +36,7 @@ int sumaFila(int D[N_FIL][N_COL],int fila) {
     return sum; 
 }
 
-void pasarTestigo(Semaphore& testigo ,int d[38], const int silla, const bool hayFila, Semaphore* r[62], const int pareja[N_EST], const bool terminado){
+void pasarTestigo(Semaphore& testigo ,int d[], const int silla, const bool hayFila, Semaphore* r[], const int pareja[], const int terminado, const bool examen_fin[]){
     
     
     for (int i = 0; i < N_EST; i++)
@@ -48,12 +48,26 @@ void pasarTestigo(Semaphore& testigo ,int d[38], const int silla, const bool hay
        }
        
     }
-    if (silla<2 && d[61]>0){
-        d[61]--;
-        r[61]->signal();
-    }else if(terminado && d[62]>0){
-        d[62]--;
-        r[62]->signal();
+    for (int i = N_EST; i < N_EST*2; i++)
+    {
+       if (examen_fin[i] && d[i]>0)
+       {
+        d[i]--;
+        r[i]->signal();
+       }
+       
+    }
+    if (silla<2 && d[120]>0){
+        d[120]--;
+        r[120]->signal();
+    
+    }else if(terminado == 30 && d[121]>0){
+        d[121]--;
+        r[121]->signal();
+    }
+    else if(silla == 2 && d[122]>0){
+        d[122]--;
+        r[122]->signal();
     }else{
         testigo.signal();
     }
@@ -61,24 +75,47 @@ void pasarTestigo(Semaphore& testigo ,int d[38], const int silla, const bool hay
 }
 
 //----------------------------------------------------
-void Estudiante(int nip,int& fila,  bool& hayFila, int D[N_FIL][N_COL], int& silla, int resultado[N_EST], bool examen_fin[N_EST],
-                int& silla1, int& silla2, int pareja[], Semaphore* r[], Semaphore& testigo, int d[36], int& terminado){
+void Estudiante(const int nip,int& fila,  bool& hayFila, int D[N_FIL][N_COL], int& silla, int resultado[], bool examen_fin[],
+                int& silla1, int& silla2, int pareja[], Semaphore* r[], Semaphore& testigo, int d[], int& terminado){
     // esperar por una silla libre
     //<await (silla<2) // una de las dos esté libre
+   
+        testigo.wait();
+        
+        if(silla>=2){
+            d[120]++;
+            testigo.signal();
+            r[120]->wait();      
+            
+        }
         silla++;
         if(silla == 0){
             silla1 = nip;
-        }else{
+            cout<<silla1;
+        }else if(silla == 1){
             silla2 = nip;
+            cout<<silla2;
         }
+        
+        pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);
+        
     //> 
     
     //esperar me sea asignada pareja y fila
    // <await (hayFila = true && pareja[nip]>=0 )
+        testigo.wait();
+    
+        if(!hayFila && pareja[nip]<0){
+            d[nip]++;
+            testigo.signal();
+            r[nip]->wait();
+            
+        }
         int miFila = fila;
         int miPareja = pareja[nip];
-        silla = 0; //>
-                
+        silla = 0; 
+        pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);//>
+         
         if (nip<miPareja) {
             // calcular máx de mi 
             //<
@@ -86,42 +123,68 @@ void Estudiante(int nip,int& fila,  bool& hayFila, int D[N_FIL][N_COL], int& sil
             resultado[nip] = maxFila(D,miFila);
             //hacérselo llegar a mi pareja
             examen_fin[nip]=true; 
-            //pasarTestigo(testigo,d,silla);//>
+            pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);//>
+            
         }
         else {
             // calcular la suma de mi fila 
             resultado[nip] = sumaFila(D, miFila);
             //coger info de max (de mi pareja) 
             //<await(examen_fin[miPareja]= true)
-            //mostrar resultados>
+            testigo.wait();
+            if(!examen_fin[miPareja]){
+                d[60+miPareja]++;
+                testigo.signal();
+                r[60+miPareja]->wait();
+            }
+            //mostrar resultados
+            pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);//>
             cout<< left << setw(6)<<miFila << "|  " <<miPareja <<"-"<< setw(4)<<nip << "|  "<< setw(7) <<resultado[miPareja]<< "|  "<<resultado[nip]<<endl;
             //comunicar finalizacíon
             //<
             testigo.wait();
             examen_fin[nip] = true;
             terminado++;
-            //pasarTestigo(testigo, d, silla);
-            //>
+            pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);//>
             //----------------------------------------------------
-
-    } 
+        } 
 }
-void Profesor (int& silla, int& silla1, int& silla2, int pareja[N_EST], int& fila, bool& hayFila, Semaphore r[], bool examen_fin[N_EST],const int terminado){
+void Profesor (int& silla, int& silla1, int& silla2, int pareja[], int& fila, bool& hayFila, Semaphore* r[], bool examen_fin[],const int terminado, Semaphore& testigo, int d[]){
+    cout<<"profesor";
+    
     for(int i=0; i<N_FIL; i++) {
+        hayFila = false;
         // esperar a que haya dos
-        //<await silla = 2>
+        //<await silla = 2
+          
+        if(silla!=2){
+            d[122]++;
+            testigo.signal();
+            r[122]->wait();
+        }
+        pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);//>
         //comunicar a cada uno su pareja, y la fila que les toca
         //<
+        
+        testigo.wait();
         pareja[silla1]= silla2;
         pareja[silla2] = silla1;
         fila = i;
-        hayFila = true; //>
+        hayFila = true; 
+        pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);//>
 
-    }
-    // pensar si mejor fin examen contador y termiando = true cuando el contador sea = 60 pq ahora habria que 
-    // esperar que todos hayan terminado
+     }
+    //pensar si mejor fin examen contador y termiando = true cuando el contador sea = 60 pq ahora habria que 
+   // esperar que todos hayan terminado
     
     //<await(terminado=30)
+        if(terminado!=30){
+            d[121]++;
+            testigo.signal();
+            r[121]->wait();
+            
+        }
+    pasarTestigo(testigo,d,silla,hayFila,r,pareja,terminado,examen_fin);
             //fin examen>
 
 }
@@ -142,6 +205,7 @@ void leerFich(int D[N_FIL][N_COL]){
 
 
 int main(){
+    cout<<"a";
     int D[N_FIL][N_COL]; //para almacenar los datos
     int fila = 0;  //cada pareja cogerá una
     int pareja[N_EST]={-1}; //pareja[i] será la pareja asignada
@@ -151,26 +215,28 @@ int main(){
     int silla1; // su valor es el del estudiante sentado
     int silla2; // su valor es el del estudiante sentado
     int resultado[N_EST];
-    bool hayFila;
-    // Semaphore* s3;
-    Semaphore* r[62];
+    bool hayFila=false;
+    //Semaphore* s3;
+    Semaphore* r[123];
     Semaphore testigo(1);
-    int d[62]= {0};
+    int d[123]= {0};
     int terminado = 0;
 
 
     //cargar "datos.txt" en "D"
     leerFich(D);
     thread P[61];
-    for(int i=0; i<N_EST; i++){
-        P[i]= thread(&Estudiante, i, ref(fila), ref(hayFila), D, ref(silla), ref(resultado), ref(examen_fin), ref(silla1), ref(silla2), ref(pareja), ref(r), ref(testigo), ref(d), ref(terminado));
-    }
-    P[60]= thread(&Profesor, ref(silla), ref(silla1), ref(silla2), ref(pareja), ref(fila), ref(hayFila), ref(examen_fin), terminado);
     
-    for (int i = 0; i < N_EST; i++){
-         P[i].join();//me bloqueo hasta que "P[i]" termine
+    P[0]= thread(&Profesor, ref(silla), ref(silla1), ref(silla2), ref(pareja), ref(fila), ref(hayFila), ref(r),ref(examen_fin), terminado,ref(testigo), ref(d));
+   
+    for(int i=1; i<=N_EST; i++){
+        P[i]= thread(&Estudiante, i-1, ref(fila), ref(hayFila), ref(D), ref(silla), ref(resultado), ref(examen_fin), ref(silla1), ref(silla2), ref(pareja), ref(r), ref(testigo) , ref(d), ref(terminado));
     }
-    P[60].join();
+    
+    P[0].join();
+    for (int i = 0; i < N_EST +1; i++){
+        P[i].join();//me bloqueo hasta que "P[i]" termine
+    }
     cout << "Prueba finalizada\n";
     return 0;
 
